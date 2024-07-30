6f66012b79a8bb87b55a82b4f719a2545bc9729b
Sergio García <168285980+cadcomnet@users.noreply.github.com>
2024-07-30 23:57:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,22 +2,19 @@ import streamlit as st
 import pandas as pd
 import folium
 from folium.plugins import MarkerCluster
-from streamlit_folium import folium_static
+from streamlit_folium import folium_static, st_folium
 from opencage.geocoder import OpenCageGeocode
 
-lista_proyectos = ["", "(SUE)" , "3D Laser Scanning" , "6370 Manor lane up to the intersection of SW 66 Avenue and SW 80 Street" ,
-                   "ALTA Survey" , "As Built Survey" , "As-Built" , "As-Built Survey" ,
-                   "As-Built SurveySR 997/Krome Ave from" , "Bathymetric Survey" , "Boundary" ,
-                   "Boundary Analysis" , "Boundary and Topographic Survey" , "Boundary Survey" ,
-                   "Collect elevations" , "Construction" , "Construction Layout" , "Elevation Certificate" ,
-                   "Engineering Services" , "Final Plat" , "Final Survey" , "Foundation Survey" ,
-                   "Legal Description" , "Map of Boundary Survey" , "Mark the property corners" ,
-                   "Miscellaneous" , "Miscellaneous Surveying" , "Photogrammetric Survey" ,
-                   "Plat" , "Re-Certification" , "Right-of-Way" , "Setback Survey" , "Sewer As-Built/Water" ,
-                   "Site Plan Survey" , "Sketch to Accompany Legal" , "stakeout" , "SUE" , "Survey" ,
-                   "Tentative Plat" , "Topographic" , "Topographic Survey" , "Unknown Point Feature" ,
-                   "Update" , "Update Alta Survey" , "Update Boundary Survey" , "Update Survey" , "Up-Date Survey"
-                    ]
+url_1 = "https://github.com/cadcomnet/data/blob/main/longitude/depurados/2019%20LONGITUDE%20PROJECTS-D.csv"
+url_2 = "https://github.com/cadcomnet/data/blob/main/longitude/depurados/2020%20LONGITUDE%20PROJECTS-D.csv"
+url_3 = "https://github.com/cadcomnet/data/blob/main/longitude/depurados/2021%20LONGITUDE%20PROJECTS-D.csv"
+url_4 = "https://github.com/cadcomnet/data/blob/main/longitude/depurados/2022%20LONGITUDE%20PROJECTS-D.csv"
+
+# codigo css y configuracion general de la pagina
+# Configurar la página en modo wide screen
+st.set_page_config(layout="wide")
+
+
 
 # Función para geocodificar una dirección utilizando OpenCage
 def geocode_address(address, key):
@@ -53,91 +50,64 @@ def mapas(datos):
             tooltip=row['LAYER']
         ).add_to(marker_cluster)
 
-    # Mostrar mapa en Streamlit
     folium_static(m)
+
+def obten_numeros_unicos(numeros):
+
+    lista_de_numeros_unicos = []
+
+    numeros_unicos = set(numeros)
+
+    for numero in numeros_unicos:
+        lista_de_numeros_unicos.append(numero)
+
+    return lista_de_numeros_unicos
+
 
 
 # Cargar datos
+st.sidebar.header("Longitude Data Projects")
+st.sidebar.text("Years 2019 - 2022")
 
-st.title("Longitude Data Projects")
-st.text("Years 2019 - 2022")
+#st.sidebar.title('Year of project')
+#st.write("Choose a year")
+#star_year = st.selectbox("Year", options=["2019", '2020', '2021', '2022'], index=0, key='Year')
+#submit_btn = st.form_submit_button("Load year", type='primary')
 
+data = pd.read_csv("depurados/2013-2022.csv",
+                   dtype={'LONGITUDE' : float,
+                          'LATITUDE' : float,
+                          'LABEL': str,
+                          'LAYER' : str,
+                         'SYMBOL': str,
+                          'KML_STYLE': str})
 
-with st.form('Year of project'):
+#lista_proyectos = data['LAYER'].to_list()
+lista_proyectos = obten_numeros_unicos(data['LAYER'].to_list())
+lista_proyectos.insert(0, '')
+data_layer = data
 
-        st.write("Choose a year")
-        star_year = st.selectbox("Year", options=["2019", '2020', '2021', '2022'], index=0, key='Year')
-        submit_btn = st.form_submit_button("Load year", type='primary')
-        col1, col2 = st.columns(2)
+# Input de búsqueda
 
+label_search = st.sidebar.text_input("Search project number:", max_chars=5, placeholder="19000")
+if label_search:
+   data = data[data['LABEL'].str.contains(label_search, case=False, na=False)]
 
-        if star_year == '2019':
-            data = pd.read_csv("depurados/2019 LONGITUDE PROJECTS-D.csv",
-                               dtype={'LONGITUDE' : float,
-                                      'LATITUDE' : float,
-                                      'LABEL': str,
-                                      'LAYER' : str,
-                                     'SYMBOL': str,
-                                      'KML_STYLE': str})
-            data_layer = data
-        if star_year == '2020':
-            data = pd.read_csv("depurados/2020 LONGITUDE PROJECTS-D.csv",
-                               dtype={'LONGITUDE' : float,
-                                      'LATITUDE' : float,
-                                      'LABEL': str,
-                                      'LAYER' : str,
-                                     'SYMBOL': str,
-                                      'KML_STYLE': str})
-            data_layer = data
-        if star_year == '2021':
-            data = pd.read_csv("depurados/2021 LONGITUDE PROJECTS-D.csv",
-                               dtype={'LONGITUDE' : float,
-                                      'LATITUDE' : float,
-                                      'LABEL': str,
-                                      'LAYER' : str,
-                                     'SYMBOL': str,
-                                      'KML_STYLE': str})
-            data_layer = data
-        if star_year == '2022':
-            data = pd.read_csv("depurados/2022 LONGITUDE PROJECTS-D.csv",
-                               dtype={'LONGITUDE' : float,
-                                      'LATITUDE' : float,
-                                      'LABEL': str,
-                                      'LAYER' : str,
-                                     'SYMBOL': str,
-                                      'KML_STYLE': str})
-            data_layer = data
-
-st.divider()
-col1, col2 = st.columns(2)
-
-with col1:
-        # Input de búsqueda
-        label_search = st.text_input("Search project number:", max_chars=5, placeholder="19000")
-        if label_search:
-           data = data[data['LABEL'].str.contains(label_search, case=False, na=False)]
-
-        # Input de búsqueda por dirección
-        address_search = st.text_input("Buscar por dirección:")
-        address_lat, address_lon = None, None
-        if address_search:
-            address_lat, address_lon = geocode_address(address_search, OPENCAGE_API_KEY)
+# Input de búsqueda por dirección
+address_search = st.sidebar.text_input("Buscar por dirección:")
+address_lat, address_lon = None, None
+if address_search:
+    address_lat, address_lon = geocode_address(address_search, OPENCAGE_API_KEY)
 
 
-        label_search2 = st.selectbox('Choose a type projects', lista_proyectos, index=0)
-        if label_search2:
-            data = data_layer[data_layer['LAYER'].str.contains(label_search2, case=False, na=False)]
 
-with col2:
-    st.write(data)
+label_search2 = st.sidebar.selectbox('Choose a type projects', lista_proyectos, index=0)
+if label_search2:
+    data = data_layer[data_layer['LAYER'].str.contains(label_search2, case=False, na=False)]
+
+
+dicc_ = { 'PROJECT' : data['LABEL'].to_list() ,  'TYPE':  data['LAYER'].to_list() }
+st.sidebar.table(dicc_)
+
 
 st.write(mapas(data))
-
-
-
-
-
-
-
-
-
